50d7625e7c2721452e0ff1af5809b228d479b3f7
ErycK <eryck@exemplo.com>
2025-10-22 00:19:47 -0300

    Simplificar validação de email no modelo User

--- backend/models/User.js
+++ backend/models/User.js
@@ -12,8 +12,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Email é obrigatório'],
     unique: true,
-    lowercase: true,
-    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Email inválido']
+    lowercase: true
   },
   password: {
     type: String,
